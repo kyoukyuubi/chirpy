ec0072b2bf9181f4362790242a07dee04b85c861
Kyou Kyuubi <194653707+kyoukyuubi@users.noreply.github.com>
2025-04-22 21:56:37 +0200

    Made a custom handler to learn how that works

--- main.go
+++ main.go
@@ -5,17 +5,24 @@ import (
 	"net/http"
 )
 
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
 	server := http.Server{
 		Handler: mux,
 		Addr: ":" + port,
 	}
+	mux.HandleFunc("/healthz", healthzHandler)
 	
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
