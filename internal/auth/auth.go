71ef6130da4742a3c2e4e65eea1cbd302c671038
Kyou Kyuubi <194653707+kyoukyuubi@users.noreply.github.com>
2025-04-26 22:21:19 +0200

    Added JWT with tests, no functionality yet

---
+++ internal/auth/auth.go
@@ -0,0 +1,65 @@
+package auth
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hash), nil
+}
+
+func CheckPasswordHash(hash, password string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer: "chirpy",
+		IssuedAt: jwt.NewNumericDate(time.Now().UTC()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(expiresIn)),
+		Subject: userID.String(),
+	})
+
+	signedToken, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", err
+	}
+	return signedToken, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(tokenSecret), nil
+	})
+
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	if claims, ok := token.Claims.(*jwt.RegisteredClaims); ok && token.Valid {
+		id, err := uuid.Parse(claims.Subject)
+		if err != nil {
+			return uuid.Nil, err
+		}
+		return id, nil
+	}
+
+	return uuid.Nil, fmt.Errorf("invalid token")
+}
--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,6 +1,11 @@
 package auth
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+)
 
 func TestHasPassword(t *testing.T) {
 	cases := []struct {
@@ -29,5 +34,66 @@ func TestHasPassword(t *testing.T) {
 		if err := CheckPasswordHash(hash, "wrongpassword"); err == nil {
 			t.Errorf("Expected wrong password to NOT match")
 		}
+	}
+}
+
+func TestJWT(t *testing.T) {
+	tokenSecret1 := "SecretTokenString123"
+	tokenSecret2 := "IamSecretLookAtMe"
+	uuid1 := uuid.New()
+	uuid2 := uuid.New()
+	uuid3 := uuid.New()
+
+	jwtString1, _ := MakeJWT(uuid1, tokenSecret1, time.Second*30)
+	jwtString2, _ := MakeJWT(uuid2, tokenSecret2, time.Second*30)
+	expiredJWT, _ := MakeJWT(uuid3, tokenSecret1, -1*time.Second)
+
+	tests := []struct {
+		name string
+		uuid uuid.UUID
+		jwtString string
+		tokenSecret string
+		wantErr bool
+	}{
+		{
+			name: "Correct jwt",
+			uuid: uuid1,
+			jwtString: jwtString1,
+			tokenSecret: tokenSecret1,
+			wantErr: false,
+		},
+		{
+			name: "Wrong jwt",
+			uuid: uuid1,
+			jwtString: jwtString2,
+			tokenSecret: tokenSecret1,
+			wantErr: true,
+		},
+		{
+			name: "Empty JWT",
+			uuid: uuid2,
+			jwtString: "",
+			tokenSecret: tokenSecret2,
+			wantErr: true,
+		},
+		{
+			name: "Expired JWT",
+			uuid: uuid3,
+			jwtString: expiredJWT,
+			tokenSecret: tokenSecret1,
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			id, err := ValidateJWT(tt.jwtString, tt.tokenSecret)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ValidateJWT() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !tt.wantErr && id != tt.uuid {
+				t.Errorf("ValidateJWT() error = %v, want id %v", err, tt.uuid)
+			}
+		})
 	}
 }
